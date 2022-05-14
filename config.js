61a11a82bd65171095898a8b62b9afbc1345f138
SmugTheKiler <emirgamaleev2009@gmail.com>
2022-05-13 22:25:58 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,98 +1,98 @@
-
 export default {
-  about: {
-    image: "https://cdn.discordapp.com/attachments/969899330091773992/974777255505043596/smugthekiler.png",
-    name: "SmugTheKiler",
-    job: "Backend Developer",
-    description: `
-    Hey! I am SmugTheKiler - Or Emir. I tought myself on how to program since
-I was about 10, and honestly, its epic. Currently in Georgia, and did this as a part of a competition.. I developed lots of things, and have a lot of people use them, without them evening knowing it was made by me 🥲
-      `,
-    socialLinks: [
+    about: {
+      image: "https://cdn.discordapp.com/attachments/969899330091773992/974777255505043596/smugthekiler.png",
+      name: "SmugTheKiler",
+      job: "Backend Developer",
+      description: `
+      Hey! I am SmugTheKiler - Or Emir. I tought myself on how to program since
+  I was about 10, and honestly, its epic. Currently in Georgia, and did this as a part of a competition.. I developed lots of things, and have a lot of people use them, without them evening knowing it was made by me 🥲
+        `,
+      socialLinks: [
+        {
+          name: "Github",
+          icon: "fab fa-github",
+          color: "gray",
+          to: "https://github.com/SmugTheKiler",
+        },
+        {
+          name: "Discord",
+          color: "indigo",
+          icon: "fab fa-discord",
+          to: "https://discord.gg/E35Mq64an9",
+        },
+        {
+          name: "Youtube",
+          color: "pink",
+          icon: "fab fa-youtube",
+          to: "https://www.youtube.com/channel/UCbOqnGLxo9t3JRrP1QL7Rkg",
+        }, /*
+        {
+          name: "Medium",
+          color: "purple",
+          icon: "fab fa-medium",
+          to: "https://medium.com/@...",
+        }, */
+      ],
+    },
+    journey: {
+      experience: "2+ Years Experience",
+      totalProjects: 4,
+    //  work: "Hobby",
+    },
+    skills: [
       {
-        name: "Github",
-        icon: "fab fa-github",
-        color: "gray",
-        to: "https://github.com/SmugTheKiler",
+        name: "JavaScript",
+        percent: 85,
+        color: "#384d54"
       },
       {
-        name: "Discord",
-        color: "indigo",
-        icon: "fab fa-discord",
-        to: "https://discord.gg/E35Mq64an9",
+        name: "NodeJS",
+        percent: 90,
+        color: "#DDC123",
       },
       {
-        name: "Youtube",
-        color: "pink",
-        icon: "fab fa-youtube",
-        to: "https://www.youtube.com/channel/UCbOqnGLxo9t3JRrP1QL7Rkg",
-      }, /*
+        name: "HTML",
+        percent: 75,
+        color: "#3C873A",
+      },
       {
-        name: "Medium",
-        color: "purple",
-        icon: "fab fa-medium",
-        to: "https://medium.com/@...",
-      }, */
+        name: "CSS",
+        percent: 45,
+        color: "#E58A3F",
+      },
+      {
+        name: "Python",
+        percent: 5,
+        color: "#2b7489"
+      }
     ],
-  },
-  journey: {
-    experience: "2+ Years Experience",
-    totalProjects: 4,
-  //  work: "Hobby",
-  },
-  skills: [
-    {
-      name: "JavaScript",
-      percent: 85,
-      color: "#384d54"
-    },
-    {
-      name: "NodeJS",
-      percent: 90,
-      color: "#DDC123",
-    },
-    {
-      name: "HTML",
-      percent: 75,
-      color: "#3C873A",
-    },
-    {
-      name: "CSS",
-      percent: 45,
-      color: "#E58A3F",
-    },
-    {
-      name: "Python",
-      percent: 5,
-      color: "#2b7489"
-    }
-  ],
-  projects: [
-    // Statusliste: NEW - 1, Coming Soon - 2, Active - 3, Maintenance - 4, It's Over - 5, Closed - 6
-    {
-      name: "${Ella}",
-      description: "An awesome Discord bot with over 200 servers, and 180k+ users using it. It helps you with a lot of stuff like moderation, fun, utility, ect.",
-      status: 2,
-      to: "https://discord.com/oauth2/authorize?client_id=743292394237329440&scope=bot&permissions=8"
-    },
-    {
-      name: "Funy API",
-      description: "A good API template for anyone to use. Has token Generation, registration, settings, multiple endpoints, and more.",
-      status: 2,
-      to: "https://github.com/SmugTheKiler/funy-api"
-    },
-    {
-      name: "${Ella}'s Website'",
-      description: "Find general info on ${Ella} on its website",
-      status: 1,
-      to: "https://ellabot.xyz"
-    },
-    {
-    // {
-    //   name: "",
-    //   description: "",
-    //   status: 1,
-    //   to: "",
-    // },    
-  ],
+    projects: [
+      // Statusliste: NEW - 1, Coming Soon - 2, Active - 3, Maintenance - 4, It's Over - 5, Closed - 6
+      {
+        name: "${Ella}",
+        description: "An awesome Discord bot with over 200 servers, and 180k+ users using it. It helps you with a lot of stuff like moderation, fun, utility, ect.",
+        status: 2,
+        to: "https://discord.com/oauth2/authorize?client_id=743292394237329440&scope=bot&permissions=8"
+      },
+      {
+        name: "Funy API",
+        description: "A good API template for anyone to use. Has token Generation, registration, settings, multiple endpoints, and more.",
+        status: 2,
+        to: "https://github.com/SmugTheKiler/funy-api"
+      },
+      {
+        name: "${Ella}'s Website'",
+        description: "Find general info on ${Ella} on its website",
+        status: 1,
+        to: "https://ellabot.xyz"
+      },
+      //{
+      // {
+      //   name: "",
+      //   description: "",
+      //   status: 1,
+      //   to: "",
+      // },    
+      //},
+  ]
 };
